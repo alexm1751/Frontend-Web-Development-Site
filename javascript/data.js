26ce9da420de907c7ab221133f7b9a795d9ac62b
alexm1751 <alex_mason1@hotmail.co.uk>
2017-11-22 22:52:00 +0000

    JSON implimented, roles and experience loop added.

---
+++ javascript/data.js
@@ -0,0 +1,16 @@
+let jsonString = `
+{ "roles": [
+  { 
+    "roleTitle":"Apple Genius" ,
+    "roleDurationDate":"November 2015 - Present",
+    "roleDescription":"Genius at the Apple Highcross store in Leicester.",
+    "roleResponsibilities":"<ul>Time Managemner</ul><ul>Time Managemner</ul><ul>Time Managemner</ul>"
+  },
+  {
+    "roleTitle":"Apple Pay Server Reliability Engineer",
+    "roleDurationDate":"July - December 2017",
+    "roleDescription":"Front line support for Server Reliability Engineer Team in Apple Pay",
+    "roleResponsibilities":"<ul>Time Managemner</ul><ul>Time Managemner</ul><ul>Time Managemner</ul>"
+  }
+]}
+`;
