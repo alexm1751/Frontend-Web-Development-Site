117ccddc3a1be3290d185020316ee144d4722032
alexm1751 <alex_mason1@hotmail.co.uk>
2017-12-29 21:36:42 +0000

    JavaScript and some CSS tweaks and minor comments

--- javascript/parse-data.js
+++ javascript/parse-data.js
@@ -1,8 +1,13 @@
 (function(){
+
+  /*creates a variable for the information to be entered into the HTML*/
   let thisData = document.getElementById("rolelist");
 
+
+  /*Creates a variable obj to read the strings of information from the JSON file*/
   let obj = JSON.parse(jsonString);
 
+  /*iterates over the array of strings printing assigning the information to the variables in theData to go into HTML*/
   for (var i = 0; i <= 2; i++) {
       let theData =  
       `<h4>${obj.rolelist[i].roleTitle}</h4>
